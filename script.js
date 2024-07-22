c7276a8cf59a5689d0f83abf2edd74d1420a841e
MacAlistair1 <lamichhaneaj@gmail.com>
2024-07-22 13:49:02 +0545

    mis community pre version is ready

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth scrolling for navigation links
+document.querySelectorAll("nav a").forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
